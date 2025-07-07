0933ef8074be99ffc08d71dec6d6ccbd9dd842d8
khaledabosaeed <kahledblata242@gmail.com.com>
2025-07-07 13:30:34 +0300

    feat: implement todo list with item details and loading states
    - Add loading, error, and not-found components
    - Create service for fetching todo data
    - Implement item list and item components with routing
    - Add type definitions for todo items
    - Update main page layout and styling

---
+++ src/app/@types/main.d.ts
@@ -0,0 +1,7 @@
+export interface ITask {
+    id: number;
+    completed: boolean;
+    todo: string;
+    userId: number
+}
+
--- src/app/_Components/home/itemList/ItemList.tsx
+++ src/app/_Components/home/itemList/ItemList.tsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import { ITask } from '@/app/@types/main';
+import { fetchData } from '@/app/services/service'
+import Item from '../item/Item';
 
-function ItemList() {
+async function ItemList() {
+  const todos: ITask[] = await fetchData();
   return (
-    <div>ItemList</div>
+    <div className="max-w-md mx-auto mt-10 p-4 bg-slate-900 text-slate-200 rounded-xl shadow-md">
+      <ul className="space-y-3">
+        {todos.map((item) => (
+          <Item key={item.id} item={item} />
+        ))}
+      </ul>
+    </div>
   )
 }
 
